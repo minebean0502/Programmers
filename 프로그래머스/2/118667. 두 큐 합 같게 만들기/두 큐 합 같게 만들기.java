63f020a2ac2efdff676b5f1bf9741082eb357129
minebean0502 <142384840+minebean0502@users.noreply.github.com>
2024-02-14 16:19:30 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 60.42 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -19,18 +19,17 @@ class Solution {
         long target = (sum1 + sum2) / 2; // 목표 합
         int moveCount = 0;
         int index1 = 0, index2 = 0;
-        int maxMove = length * 2; // 최대 이동 가능 횟수
 
         while (sum1 != sum2) {
             // 두 큐의 합이 같지 않고, 최대 이동 횟수를 넘지 않은 경우
-            if (sum1 > sum2 && index1 < maxMove) {
+            if (sum1 > sum2 ) {
                 // 큐1에서 빼서 큐2에 넣기
                 int num = q1.poll();
                 q2.offer(num);
                 sum1 -= num;
                 sum2 += num;
                 index1++;
-            } else if (sum1 < sum2 && index2 < maxMove) {
+            } else if (sum1 < sum2) {
                 // 큐2에서 빼서 큐1에 넣기
                 int num = q2.poll();
                 q1.offer(num);
