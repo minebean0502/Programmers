6a7a10c28f295c9fd0f8db07a902002cda5ef63a
minebean0502 <142384840+minebean0502@users.noreply.github.com>
2024-07-11 00:35:42 +0900

    [Silver II] Title: 키로거, Time: 928 ms, Memory: 204572 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -6,29 +6,29 @@ import java.util.ListIterator;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
-    static LinkedList<String> list;
-    static ListIterator<String> iterator;
-    static String[] inputs;
+    static LinkedList<Character> list;
+    static ListIterator<Character> iterator;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int inputNum = Integer.parseInt(br.readLine());
         for (int i = 0; i < inputNum; i++) {
             list = new LinkedList<>();
             iterator = list.listIterator();
-            inputs = br.readLine().split("");
-            for (String str : inputs) {
+            String inputs = br.readLine();
+            for (Character str : inputs.toCharArray()) {
                 switch (str) {
-                    case "<" :
+                    case '<' :
                         if (iterator.hasPrevious()) {
                             iterator.previous();
                         }
                         break;
-                    case ">" :
+                    case '>' :
                         if (iterator.hasNext()) {
                             iterator.next();
                         }
                         break;
-                    case "-" :
+                    case '-' :
                         if (iterator.hasPrevious()) {
                             iterator.previous();
                             iterator.remove();
@@ -39,7 +39,7 @@ public class Main {
                         break;
                 }
             }
-            for (String str : list) {
+            for (Character str : list) {
                 sb.append(str);
             }
             sb.append("\n");
